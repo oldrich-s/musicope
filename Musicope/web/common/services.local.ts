18d166203ef1bbbe3ddd4062f3d60627c7d3724d
Oldrich Svec <oldrich.svec@gmail.com>
2013-02-03 15:53:33 +0100

    templates

---
+++ Musicope/web/common/services.local.ts
@@ -0,0 +1,19 @@
+
+export function get(name: string, defaultValue: any) {
+  var val = localStorage.getItem(name);
+  if (!val || val === "undefined") {
+    return defaultValue;
+  } else {
+    try {
+      return JSON.parse(val);
+    } catch (e) {
+      return val;
+    }
+    
+  }
+}
+
+export function set (name: string, value: any) {
+  var val = JSON.stringify(value);
+  localStorage.setItem(name, val);
+}
--- Musicope/web/list/_main.ts
+++ Musicope/web/list/_main.ts
@@ -2,6 +2,7 @@
 
 import defParams = module("./_paramsDefault");
 import paramService = module("../common/services.params");
+import local = module("../common/services.local");
 import inputs = module("./inputs/_load");
 import lastPlayedSongs = module("./lastPlayedSongs");
 
@@ -14,21 +15,22 @@ var o: AppViewModel;
 
 class AppViewModel {
 
-  filteredSongs = ko.observableArray();
-  searchQuery = ko.observable(localStorage.getItem("query") || "");
-  gameParams = ko.observable(localStorage.getItem("params") || "");
+  templates: KnockoutObservableArray;
+  selectedTemplate: KnockoutObservableString;
+  filteredSongs: KnockoutObservableArray;
+  searchQuery: KnockoutObservableString;
+  gameParams;
   private songs: ISong[] = [];
   private pouch: ph.DB;
 
   constructor() {
     o = this;
-
-    o.gameParams.subscribe((params) => { localStorage.setItem("params", params); });
-    o.searchQuery.subscribe((query) => { localStorage.setItem("query", query); });
-
+    o.initTemplates();
+    o.initSelectedTemplate();
+    o.initGameParams();
+    o.initSearchQuery();
     o.initFilteredSongs();
     o.loadSongs();
-
   }
 
   redirect(song: ISong) {
@@ -37,6 +39,47 @@ class AppViewModel {
       if (pars.charAt(0) !== "&") { pars = "&" + pars; }
       window.location.href = "../game/index.html?c_songUrl=" + decodeURIComponent(song.url) + pars;
     });
+  }
+
+  private initTemplates() {
+    var o = this;
+    var defaultTemplates = [
+      {name: "left", value: "p_deviceIn=0&p_deviceOut=1&p_waits=[0,0]&p_volumes=[0,1]"},
+      {name: "right", value: "p_deviceIn=0&p_deviceOut=1&p_waits=[0,0]&p_volumes=[1,0]"},
+      {name: "both", value: "p_deviceIn=0&p_deviceOut=1&p_waits=[0,0]&p_volumes=[0,0]"},
+      {name: "waitLeft", value: "p_deviceIn=0&p_deviceOut=1&p_waits=[1,1]&p_volumes=[0,1]"},
+      {name: "waitRight", value: "p_deviceIn=0&p_deviceOut=1&p_waits=[1,1]&p_volumes=[1,0]"},
+      {name: "waitBoth", value: "p_deviceIn=0&p_deviceOut=1&p_waits=[1,1]&p_volumes=[0,0]"}
+    ];
+    var templates = local.get("templates", defaultTemplates);
+    o.templates = ko.observableArray(templates);
+    o.templates.subscribe((templates) => {
+      local.set("templates", JSON.stringify(templates));
+    });
+  }
+
+  private initSelectedTemplate() {
+    var template = local.get("selectedTemplate", "");
+    o.selectedTemplate = ko.observable(template);
+    o.selectedTemplate.subscribe((template) => {
+      local.set("selectedTemplate", template);
+    });
+  }
+
+  private initGameParams() {
+    o.gameParams = ko.computed(() => {
+      var selectedName = o.selectedTemplate();
+      var selectedTemplates = o.templates().filter((templ) => {
+        return templ["name"] == selectedName;
+      });
+      return selectedTemplates[0]["value"];
+    });
+    o.gameParams.subscribe((params) => { local.set("params", params); });
+  }
+
+  private initSearchQuery() {
+    o.searchQuery = ko.observable(local.get("query", ""));
+    o.searchQuery.subscribe((query) => { local.set("query", query); });
   }
 
   private loadSongs() {
@@ -61,6 +104,7 @@ class AppViewModel {
   }
 
   private initFilteredSongs() {
+    o.filteredSongs = ko.observableArray();
     ko.computed(function () {
       var query: string = o.searchQuery();
       if (query.length == 0) {
