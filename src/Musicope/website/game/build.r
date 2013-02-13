56c732074f38dc6f1222f0f711c8481af46b1692
Oldrich Svec <oldrich.svec@gmail.com>
2013-02-13 15:01:15 +0100

    implementing r.js

---
+++ src/Musicope/website/game/build.r
@@ -0,0 +1,6 @@
+({
+    baseUrl: ".",
+    name: "_index",
+    out: "_indexb.js",
+    optimize: "none"
+})
--- src/Musicope/website/game/inputs/keyboard/actions/_load.ts
+++ src/Musicope/website/game/inputs/keyboard/actions/_load.ts
@@ -1,3 +1,27 @@
 
+import metronomeOnM = module("./metronomeOn");
+export var metronomeOn: IGame.IKeyboardActionsNew = metronomeOnM.metronomeOn;
+
+import moveBackM = module("./moveBack");
+export var moveBack: IGame.IKeyboardActionsNew = moveBackM.moveBack;
+
+import moveForwardM = module("./moveForward");
+export var moveForward: IGame.IKeyboardActionsNew = moveForwardM.moveForward;
+
+import moveHomeM = module("./moveHome");
+export var moveHome: IGame.IKeyboardActionsNew = moveHomeM.moveHome;
+
+import pauseOnM = module("./pauseOn");
+export var pauseOn: IGame.IKeyboardActionsNew = pauseOnM.pauseOn;
+
+import slowDownM = module("./slowDown");
+export var slowDown: IGame.IKeyboardActionsNew = slowDownM.slowDown;
+
+import speedUpM = module("./speedUp");
+export var speedUp: IGame.IKeyboardActionsNew = speedUpM.speedUp;
+
 import userHandsM = module("./userHands");
 export var userHands: IGame.IKeyboardActionsNew = userHandsM.userHands;
+
+import waitOnM = module("./waitOn");
+export var waitOn: IGame.IKeyboardActionsNew = waitOnM.waitOn;
