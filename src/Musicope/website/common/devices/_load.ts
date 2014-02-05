85db0b51a1c48df2a2cd0e4a15cbb3aa5a726b59
Oldrich <oldrich.svec@gmail.com>
2014-02-05 19:58:22 +0100

    working on 2.0

--- src/Musicope/website/common/devices/_load.ts
+++
@@ -1,6 +0,0 @@
-
-import jazzM = module("./jazz/_main");
-export var Jazz: IDeviceNew = jazzM.Jazz;
-
-import emptyM = module("./empty/_main");
-export var Empty: IDeviceNew = emptyM.Empty;
---
+++ src/Musicope/website/common/devices/empty.ts
@@ -0,0 +1,27 @@
+module Musicope.Devices {
+
+  export class Empty implements IDevice {
+
+    constructor() { }
+
+    inOpen(nameOrIndex, callback) { }
+
+    inClose() { }
+
+    inList() { return [""]; }
+
+    exists() { return true; }
+
+    out(byte1: number, byte2: number, byte3: number) { }
+
+    outClose() { }
+
+    outList() { return [""]; }
+
+    outOpen(name) { }
+
+    time() { return Date.now(); }
+
+  }
+
+}
