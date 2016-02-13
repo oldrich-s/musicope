c897fc6f6da2b8fec5ebaa133dffab71a0109c4f
oldrich <oldrich.svec@gmail.com>
2016-02-13 18:26:27 +0100

    removed bower, updated libs

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var clean = require('gulp-clean');
+var rimraf = require('gulp-rimraf');
 var ts = require('gulp-typescript');
 var runSequence = require('run-sequence');
 var electron = require('gulp-atom-electron');
@@ -8,72 +8,72 @@ var electron = require('gulp-atom-electron');
 var base = 'build/web/';
 
 var vendorScriptFiles = [
-	'bower_components/jquery/dist/jquery.js',
-	'bower_components/Framework7/dist/js/framework7.js',
-	'bower_components/mousetrap/mousetrap.js',
-	'bower_components/urijs/src/URI.js',
-	'src/vendor-scripts/**/*.js'
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/framework7/dist/js/framework7.js',
+    'node_modules/mousetrap/mousetrap.js',
+    'node_modules/urijs/src/URI.js',
+    'src/vendor-scripts/**/*.js'
 ];
 
 var vendorCSSFiles = [
-	'bower_components/Framework7/dist/css/framework7.ios.css',
-	'bower_components/Framework7/dist/css/framework7.ios.colors.css'
+    'node_modules/framework7/dist/css/framework7.ios.css',
+    'node_modules/framework7/dist/css/framework7.ios.colors.css'
 ];
 
 var tsFiles = [
-	'typings/**/*.d.ts',
-	'src/typescripts/**/*.ts'
+    'typings/**/*.d.ts',
+    'src/typescripts/**/*.ts'
 ];
 
 var tsProject = ts.createProject({
-	noExternalResolve: true,
-	target: 'ES5'
+    noExternalResolve: true,
+    target: 'ES5'
 });
 
 var electronOptions = {
-	version: '0.35.4',
-	platform: 'win32',
-	winIcon: './src/content/icon.ico'
+    version: '0.36.7',
+    platform: 'win32',
+    winIcon: './src/content/icon.ico'
 };
 
 gulp.task('clean', function () {
-	return gulp.src(base).pipe(clean());
+    return gulp.src(base).pipe(rimraf());
 })
 
-gulp.task('bowerScripts', function () {
-	return gulp.src(vendorScriptFiles)
-		.pipe(concat('vendor.js'))
-		.pipe(gulp.dest(base));
+gulp.task('vendorScripts', function () {
+    return gulp.src(vendorScriptFiles)
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest(base));
 });
 
-gulp.task('bowerCSS', function () {
-	return gulp.src(vendorCSSFiles)
-		.pipe(concat('vendor.css'))
-		.pipe(gulp.dest(base));
+gulp.task('vendorCSS', function () {
+    return gulp.src(vendorCSSFiles)
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest(base));
 });
 
 gulp.task('content', function (done) {
-	return gulp.src('src/content/**/*.*')
-		.pipe(gulp.dest(base));
+    return gulp.src('src/content/**/*.*')
+        .pipe(gulp.dest(base));
 });
 
 gulp.task('ts', function () {
-	return gulp.src(tsFiles)
-		.pipe(ts(tsProject))
-		.js.pipe(concat('app.js'))
-		.pipe(gulp.dest(base))
-});
-
-gulp.task('electron', function () {
-	return gulp.src('build/web/**')
-		.pipe(electron(electronOptions))
-        .pipe(gulp.dest('build/app'));
+    return gulp.src(tsFiles)
+        .pipe(ts(tsProject))
+        .js.pipe(concat('app.js'))
+        .pipe(gulp.dest(base))
 });
 
 gulp.task('copy', function () {
-	return gulp.src('build/web/**').pipe(gulp.dest('build/app/resources/app/'));
+    return gulp.src('build/web/**').pipe(gulp.dest('build/app/resources/app/'));
+});
+
+gulp.task('electron', function () {
+    return gulp.src('build/web/**')
+        .pipe(electron(electronOptions))
+        .pipe(gulp.dest('build/app'));
 });
 
 gulp.task('default', function () {
-	return runSequence('clean', ['bowerScripts', 'bowerCSS', 'ts', 'content'], 'copy');
+    return runSequence('clean', ['vendorScripts', 'vendorCSS', 'ts', 'content'], 'copy');
 });
